66c2d7a245e99ea46efa454d62349270e6347c02
Yeminting <yeminting_china@hotmail.com>
2017-04-13 07:11:11 +0800

    add template design pattern

--- template/template.go
+++ template/template.go
@@ -1,3 +1,53 @@
 package template
 
 import "fmt"
+
+type IAbstract interface {
+	AbstractMethod1()
+	AbstractMethod2()
+}
+
+type AbstractClass struct {
+}
+
+func (*AbstractClass) AbstractMethod1() {
+
+}
+func (*AbstractClass) AbstractMethod2() {
+
+}
+
+func (a *AbstractClass) TemplateMethod(c IAbstract) {
+	c.AbstractMethod1()
+	c.AbstractMethod2()
+}
+
+type ContreteA struct {
+	AbstractClass
+}
+
+func (*ContreteA) AbstractMethod1() {
+	fmt.Println("ContreteA AbstractMethod1 is call")
+}
+
+func (*ContreteA) AbstractMethod2() {
+	fmt.Println("ContreteA AbstractMethod2 is call")
+}
+
+type ContreteB struct {
+	AbstractClass
+}
+
+func (*ContreteB) AbstractMethod1() {
+	fmt.Println("ContreteB AbstractMethod1 is call")
+}
+
+func (*ContreteB) AbstractMethod2() {
+	fmt.Println("ContreteB AbstractMethod2 is call")
+}
+
+//TemplateMethod if override TemplateMethod what happend.
+func (b *ContreteB) TemplateMethod(a IAbstract) {
+	b.AbstractMethod2()
+	b.AbstractMethod1()
+}
--- template/template_test.go
+++ template/template_test.go
@@ -1,3 +1,22 @@
 package template
 
 import "testing"
+
+func TestTemplate(t *testing.T) {
+	abstractA := new(ContreteA)
+	abstractA.TemplateMethod(abstractA)
+	//out
+	//ContreteA AbstractMethod1 is call
+	//ContreteA AbstractMethod2 is call
+
+	abstractB := new(ContreteB)
+	abstractB.TemplateMethod(abstractB)
+	//out
+	//ContreteB AbstractMethod1 is call
+	//ContreteB AbstractMethod2 is call
+
+	//if override TemplateMethod
+	//out
+	//ContreteB AbstractMethod2 is call
+	//ContreteB AbstractMethod1 is call
+}
